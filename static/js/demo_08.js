af3aeaf1e16c1a3e1018a5498ff2d72eac0ccb2e
zzz <wzk1205434581@hotmail.com>
2020-12-27 22:31:58 +0800

    progress

---
+++ static/js/demo_08.js
@@ -0,0 +1,44 @@
+
+// let now = new Date();
+// let currentYear = now.getFullYear();
+// let start = new Date(currentYear, 0, 1);
+// let end = new Date(currentYear, 11, 31);
+// let current = now - start;
+// let sum = end - start;
+// let percent = current / sum;
+// console.log(percent);
+// console.log(start);
+// console.log(now);
+// console.log(end);
+
+function calcYearProgress() {
+
+
+    let now = new Date();
+    let currentYear = now.getFullYear();
+    let start = new Date(currentYear, 0, 1);
+    let end = new Date(currentYear, 11, 31);
+    let current = now - start;
+    let sum = end - start;
+    let percent = current / sum;
+    return (percent * 100) + "%";
+
+}
+
+function setYearProgress() {
+
+    $(".progress-bar").css("width", calcYearProgress);
+    $(".progress-bar").text(calcYearProgress);
+}
+
+$(document).ready(
+
+    setInterval("setYearProgress()", 1000),
+    console.log("You are such disgusting.🤑")
+
+)
+
+
+
+
+
